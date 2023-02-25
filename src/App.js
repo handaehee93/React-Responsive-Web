1ad8b5e4da7aa23538d6554ded8e3e69e30e0ad9
handaehee93 <hdh8990@naver.com>
2023-02-25 19:18:47 +0900

    Section1컴포넌트 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './reset.css'
 import './App.css';
 import Header from './componets/Header';
-
+import Section1 from './componets/Section1';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section1 />
     </div>
   );
 }
