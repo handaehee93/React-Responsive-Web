db3525871144ca6c0811e77905d00ad30605d61d
handaehee93 <hdh8990@naver.com>
2023-02-26 16:55:02 +0900

    <Section4 컴포넌트 완성>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './componets/Header';
 import Section1 from './componets/Section1';
 import Section2 from './componets/Section2';
 import Section3 from './componets/Section3';
+import Section4 from './componets/Section4';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
---
+++ src/componets/Section4.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function Section4() {
+  return (
+    <div className='section4'>
+      <div className="inner">
+        <div className="text_wrap">
+          <h2>Section4</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sint consectetur, 
+            </p>
+            <a href="#">
+              <span>button</span>
+            </a>
+        </div>
+        <ul className="images">
+          <li>
+            <a href="">
+              <h4>title</h4>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, repellat!
+              </p>
+              <div className="image">
+                <img src="" alt="" />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <h4>title</h4>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, repellat!
+              </p>
+              <div className="image">
+                <img src="" alt="" />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <h4>title</h4>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, repellat!
+              </p>
+              <div className="image">
+                <img src="" alt="" />
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <h4>title</h4>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, repellat!
+              </p>
+              <div className="image">
+                <img src="" alt="" />
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
